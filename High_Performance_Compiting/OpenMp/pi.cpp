900c1f1082ab11f54dac033b3e58a115eb5a0e5c
Isaac <jrubianom@unal.edu.co>
2021-05-12 15:11:49 -0500

    Archivos OMP practica

---
+++ High_Performance_Compiting/OpenMp/pi.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<cstdio>
+#include "omp.h"
+
+static long num_steps = 1000000;
+double step;
+int main(int argc,char** argv)
+{
+    int NUM_THREADS = std::atoi(argv[1]);
+    double pi=0.0,sum[NUM_THREADS],t2,t1; int nthreads;
+    step = 1.0/(double) num_steps;
+    omp_set_num_threads(NUM_THREADS);
+    t1 = omp_get_wtime();
+#pragma omp parallel
+{
+
+    double x; int ID,nthrds;
+    ID = omp_get_thread_num();
+    nthrds = omp_get_num_threads();
+    if(ID==0) nthreads = nthrds;
+    sum[ID]=0.0;
+    for(int i=ID;i<num_steps;i = i+nthrds){
+        x = (i+0.5)*step;
+        sum[ID] += 4.0/(1.0+x*x);
+    }
+}
+t2 = omp_get_wtime();
+for(int j = 0; j<nthreads;j++){
+     pi += step*sum[j];
+}
+std::cout<<"PI:\t"<<pi<<"\tTime:\t"<<t2-t1<<std::endl;
+return 0;
+
+}
